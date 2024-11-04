b34efbb77c69bc98e707fd9788b3d10dd65e9946
liuhuacong <Liuhuacong8>
2024-11-04 18:50:04 +0800

    公共响应参数

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,4 +1,29 @@
 
+
+export type Result = {
+  code: number;
+  message: string
+  data?: any
+  request_id: string
+};
+
+export type ResultTable = {
+  code: number
+  message: string
+  data?: {
+    /** 列表数据 */
+    list: Array<any>
+    /** 总条目数 */
+    total?: number
+    /** 每页显示条目个数 */
+    pageSize?: number
+    /** 当前页数 */
+    currentPage?: number
+  }
+  request_id: string
+};
+
+
 export const baseApiUrl = (url:string) => {
   return `/api${url}`
 }
