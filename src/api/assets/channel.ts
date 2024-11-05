dc1c8e84efced0515148227c15a3c5ec358f1e23
liuhuacong <Liuhuacong8>
2024-11-05 16:22:54 +0800

    媒体渠道

---
+++ src/api/assets/channel.ts
@@ -0,0 +1,24 @@
+import { http } from "@/utils/http";
+import {baseApiUrl,Result,ResultTable} from "@/api/utils"
+
+/** 游戏管理列表 */
+export const getChannelList = (data?: object) => {
+  return http.request<ResultTable>("post", baseApiUrl("/assets/v1/channel/list1"), { data });
+};
+
+/** 新增应用 */
+export const createChannel = (data?: object) => {
+  return http.request<ResultTable>("post", baseApiUrl("/assets/v1/channel/create"), { data });
+};
+
+/** 更新应用 */
+export const updateChannel = (data?: object) => {
+  return http.request<ResultTable>("post", baseApiUrl("/assets/v1/channel/update"), { data });
+};
+
+/** 获取所有应用 */
+export const getAllChannelList = () => {
+  return http.request<Result>("get",  baseApiUrl("/assets/v1/channel/list-all"));
+};
+
+
---
+++ src/views/assets/channel/form.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { formRules } from "./utils/rule";
+import { FormProps } from "./utils/types";
+
+const props = withDefaults(defineProps<FormProps>(), {
+  formInline: () => ({
+    id:'',
+    name: "",
+    code: "",
+    remark: ""
+  })
+});
+
+const ruleFormRef = ref();
+const newFormInline = ref(props.formInline);
+
+function getRef() {
+  return ruleFormRef.value;
+}
+
+defineExpose({ getRef });
+</script>
+
+<template>
+  <el-form
+    ref="ruleFormRef"
+    :model="newFormInline"
+    :rules="formRules"
+    label-width="82px"
+    :inline="true"
+  >
+    <el-form-item label="渠道名称" prop="name">
+      <el-input
+        v-model="newFormInline.name"
+        clearable
+        placeholder="请输入渠道名称"
+        class="!w-[300px]"
+      />
+    </el-form-item>
+
+    <el-form-item label="渠道标识" prop="code">
+      <el-input
+        v-model="newFormInline.code"
+        clearable
+        placeholder="请输入渠道称"
+        class="!w-[300px]"
+      />
+    </el-form-item>
+
+    <el-form-item label="对接文档">
+      <el-input
+        v-model="newFormInline.doc"
+        clearable
+        placeholder="请输入渠道称"
+        class="!w-[300px]"
+      />
+    </el-form-item>
+
+    <el-form-item label="备注">
+      <el-input
+        v-model="newFormInline.remark"
+        placeholder="请输入备注信息"
+        type="textarea"
+        class="!w-[300px]"
+      />
+    </el-form-item>
+  </el-form>
+</template>
